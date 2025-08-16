d1d6dab3c826124ea72478ee739ab3164946e596
Dino Bellugi <dino@berkeley.edu>
2025-08-16 07:43:41 -0700

    Remove deleted files

--- README.md
+++
@@ -1,2 +0,0 @@
-# COEQWAL
-Notebooks & importable modules for metrics calculations, plotting functions, and data extraction used by https://coeqwal.berkeley.edu/
--- notebooks/coeqwalpackage/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name = 'coeqwal-libraries',
-    packages = find_packages(include=['coeqwalpackages']),
-    version='0.1.0',
-    description='Python Library built from existing funcs to mainstream calculations',
-    author='Claire,Maram,Ava',
-)
